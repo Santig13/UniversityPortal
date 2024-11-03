4e8ddbb7de5b326d1c69018d04593af73df3d4a3
Santig13 <117572417+Santig13@users.noreply.github.com>
2024-11-03 14:36:54 +0100

    front de inicio de sesion y registro empezado
    los estilos/tamaños de inicio de sesion están mas o menos cuadrados mientras que los de regristro le queda un poco aun, además no esta ni planteado el tema de mostrar facultades.

---
+++ app.js
@@ -0,0 +1,38 @@
+const mysql=require('mysql');
+const express=require('express');
+const path=require('path');
+
+//Configuración del servidor
+const app= express();
+const port=3000;
+
+//Configuración de la base de datos
+const pool=mysql.createPool({
+    host: "localhost",
+    user: "root",
+    port: 3306,
+    password: "",
+    database: "AW_24"
+});
+
+
+// Middleware para parsear datos de formularioy aceptar JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Middleware para que cargue todos los archivos estáticos y poder usar js y css en el html
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Navegacion a la pagina de inicio
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public','Inicio.html'));
+});
+//Navegacion a la pagina de registro
+app.get('/registro',(req,res)=>{
+    res.sendFile(path.join(__dirname,'public','Registro.html'));
+});
+
+
+app.listen(port,()=>{
+    console.log('Servidor escuchando en http://localhost:${port}');
+});
