1ab887cda8b738f1aed9ecf3e369be160ca43d77
Santig13 <117572417+Santig13@users.noreply.github.com>
2024-11-03 19:12:18 +0100

    posible estructura MVC pero ns si el profe lo quiere

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const mysql=require('mysql');
 const express=require('express');
 const path=require('path');
+const { nextTick } = require('process');
 
 //Configuración del servidor
 const app= express();
@@ -23,6 +24,16 @@ app.use(express.urlencoded({ extended: true }));
 // Middleware para que cargue todos los archivos estáticos y poder usar js y css en el html
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Configurar el motor de plantillas EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//Middleware para  el manejo de errores
+app.use((err,req,res,next)=>{
+    console.error(err.stack);
+    res.status(500).send('Algo salió mal');//Temporal, hay que tratar los errores de forma adecuada
+});
+
 //Navegacion a la pagina de inicio
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'public','Inicio.html'));
@@ -31,7 +42,28 @@ app.get('/',(req,res)=>{
 app.get('/registro',(req,res)=>{
     res.sendFile(path.join(__dirname,'public','Registro.html'));
 });
+app.get('/login',(req,res)=>{
+    const {email, password}=req.body;
+    
+    if(!email || !password)
+        res.status(400).send('Faltan datos');
 
+    pool.getConnection((err,connection)=>{
+        if(err) 
+            next(err);
+        connection.query('SELECT * FROM usuarios WHERE email=? AND password=?',[email,password],(err,rows)=>{
+            connection.release();
+            if(err)  
+                next(err);
+            if(rows.length>0){
+                res.render('Inicio',{data:rows[0]});
+            }else{
+                res.status(400).send('Email o contraseña incorrectos');
+            }
+        });
+    });
+    
+});
 
 app.listen(port,()=>{
     console.log('Servidor escuchando en http://localhost:${port}');
---
+++ routes/rutas.js
@@ -0,0 +1,12 @@
+/*import exp from 'constants';
+import {controller} from '../controllers/controller.js';
+import {Router} from 'express';
+
+export const router=Router();
+
+router.get('/',controller.getInicio);
+router.get('/registro',controller.getRegistro);
+
+router.get('/login',controller.getUsuario);
+
+router.post('/registro',controller.crearUsuario);*/
