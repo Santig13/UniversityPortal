b56dbe9c14c360c180ff2c4731710992260a24aa
Santig13 <117572417+Santig13@users.noreply.github.com>
2024-11-03 20:24:55 +0100

    schemas de validacion user

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const mysql=require('mysql');
 const express=require('express');
 const path=require('path');
-const { nextTick } = require('process');
+const { z } = require('zod');
+
 
 //Configuración del servidor
 const app= express();
@@ -27,6 +28,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Configurar el motor de plantillas EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+// Esquema de validación
+const userSchema = z.object({
+    nombre: z.string({ required_error: 'Nombre es requerido' }).nonempty('Contraseña es requerida'),
+    email: z.string({ required_error: 'Email es requerido' }).email('Email no tiene el formato correcto').refine((email, ctx) => {
+        const facultad = ctx.parent.facultad;//Uso el objeto ctx(contexto en zod) para acceder a la facultad
+        const emailRegex = new RegExp(`.*@${facultad}\.com$`);
+        return emailRegex.test(email);
+    }, {
+        message: 'Email no tiene el formato correcto para la facultad'
+    }),
+    telefono: z.string({ required_error: 'Teléfono es requerido' })
+        .min(10, 'Teléfono debe tener al menos 9 caracteres'),
+    facultad: z.string({ required_error: 'Facultad es requerido' }).nonempty('Contraseña es requerida'),
+    password: z.string()
+        .nonempty('Contraseña es requerida')
+        .min(8, 'Contraseña debe tener al menos 8 caracteres')
+});
+
+const validate = (schema) => (req, res, next) => {
+    const { error } = schema.validate(req.body);
+    if (error) {
+      return res.status(400).send(error.details[0].message);
+    }
+    next();
+  };
 
 //Middleware para  el manejo de errores
 app.use((err,req,res,next)=>{
@@ -60,6 +87,31 @@ app.get('/login',(req,res)=>{
             }else{
                 res.status(400).send('Email o contraseña incorrectos');
             }
+        });
+    });
+    
+});
+//Registro de usuario
+app.post('/registro', validate(userSchema),(req,res)=>{
+    /*Aqui se valida con el middleware
+    const {nombre, email, telefono,facultad, password}=req.body;
+    
+    if(!nombre || !email || !password || !telefono || !facultad)
+        res.status(400).send('Faltan datos');
+
+    const emailRegex = new RegExp(`.*@${facultad}\.com$`);
+    if (!emailRegex.test(email)) {
+        return res.status(400).send('Email no tiene el formato correcto');
+    }*/
+
+    pool.getConnection((err,connection)=>{
+        if(err) 
+            next(err);
+        connection.query('INSERT INTO usuarios(nombre,email,password) VALUES(?,?,?)',[nombre,email,password],(err,rows)=>{
+            connection.release();
+            if(err)  
+                next(err);
+            res.render('Inicio',{data:req.body});
         });
     });
     
