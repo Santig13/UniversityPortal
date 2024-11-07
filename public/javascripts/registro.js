111a7a2e4f952133f44bce53b1e57b7d8795c53e
Alejandro Vida <75375492+AlexVRC3@users.noreply.github.com>
2024-11-07 01:29:04 +0100

    header y footer como ejs para ser reutilizados

---
+++ public/javascripts/registro.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', async function(params) {
+    // cargar facultades
+    try {
+        const respuesta = await fetch('/facultades');
+        const data = await respuesta.json();
+        const facultadSelect = document.getElementById('facultad');
+        data.forEach(facultad => {
+            const option = document.createElement('option');
+            option.value = facultad.nombre;
+            option.textContent = facultad.nombre;
+            facultadSelect.appendChild(option);
+            
+        });
+    } catch (error) {
+        console.log(error);
+    }
+
+    // cambiar el prefijo del tlf
+    document.querySelectorAll('.dropdown-item').forEach(item => {
+        item.addEventListener('click', function(e) {
+            e.preventDefault();
+            const prefix = this.getAttribute('data-prefix');
+            document.getElementById('dropdownMenuButton').textContent = prefix;
+        });
+    });
+
+    // añadir prefijo al telefono
+    const form = document.querySelector('form');
+    form.addEventListener('submit', function(e) {
+        const prefix = document.getElementById('dropdownMenuButton').textContent;
+        const telefono = document.getElementById('telefono').value;
+        document.getElementById('telefonoCompleto').value = prefix + telefono;
+    });
+});
